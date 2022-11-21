924e11ab8b30ac1cebae7415c12b94086c1a215e
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2022-11-21 18:11:09 +0100

    setting up structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CpeComponent } from './cpe/cpe.component';
+import { HeaderComponent } from './cpe/header/header.component';
+import { BodyComponent } from './cpe/body/body.component';
+import { FooterComponent } from './cpe/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CpeComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cpe/cpe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cpe',
+  templateUrl: './cpe.component.html',
+  styleUrls: ['./cpe.component.scss']
+})
+export class CpeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
