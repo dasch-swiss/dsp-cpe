99cef1c73652fa9ad17557c20ff2a632f435c605
domsteinbach <dominique.steinbach@unibas.ch>
2022-11-30 11:06:53 +0100

    feat: limit purge to gui-elements-module, define colors, limit the use of tailwinds default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // "./src/**/*.{html, ts}", // all code gets scanned ...
-    "./src/app/tailwind/*.ts" // only tw classes from the tailwind module get scanned for minifying/purging
+    //"./src/**/*.{html, ts}", // all code gets scanned ...
+    "./src/app/gui-elements-module/**/*.{html, ts}" // only tw classes from the guielements module get scanned for minifying/purging
+  ],
+  safelist: [
+    // 'bg-primary-700'
   ],
   theme: {
-    extend: {},
+
+    // remove for overriding the default tw colors - not extending
+      colors: {
+        inherit: 'inherit',
+        // name_in_template:preset
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        midnightBlue: { // our midnight-blue
+          50: '#EFF1FF'
+        },
+        primary: colors.blue,
+        gray: colors.gray
+      }
   },
   plugins: [],
 }
