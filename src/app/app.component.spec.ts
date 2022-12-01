4f01b256e6254723c95c5d679aae60e31c68619e
mdelez <60604010+mdelez@users.noreply.github.com>
2022-12-01 11:03:26 +0100

    test disabled unit test (should fail)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  fit(`should have as title 'dsp-cpe'`, () => {
+  xit(`should have as title 'dsp-cpe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('dsp-cpe');
