ec062b1601447af4e0377ca0408e35818f0d34ed
mdelez <60604010+mdelez@users.noreply.github.com>
2022-12-01 11:02:20 +0100

    test exclusive unit test (should fail)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dsp-cpe'`, () => {
+  fit(`should have as title 'dsp-cpe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('dsp-cpe');
