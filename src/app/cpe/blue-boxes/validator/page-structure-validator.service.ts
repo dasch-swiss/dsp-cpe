f6b229d94ed3fe739df92f941783650001bac3a5
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2022-12-04 17:52:04 +0100

    validation examples

--- src/app/cpe/blue-boxes/validator/page-structure-validator.service.ts
+++ src/app/cpe/blue-boxes/validator/page-structure-validator.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {PageStructure} from "../model/page-data-structure";
+import {PageStructure} from '../model/page-data-structure';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +9,32 @@ export class PageStructureValidatorService {
 
   validate(pageStructure: PageStructure) {
     // Example rule 1: It should have at least a header, a body or a footer
+    if (pageStructure.page.header == undefined && pageStructure.page.footer == undefined && pageStructure.page.body == undefined) {
+      throw new Error('It should have at least a header, a body or a footer');
+    }
 
-    // Example rule 2: If there is a header, it should contain at least one widget or not more than 3 widget
+    // Example rule 2: If there is a header, it should contain at least one widget and not more than 3 widget
+    if (pageStructure.page.header) {
+      const amountWidget = pageStructure.page.header.widgets.length;
+      if (amountWidget < 1 || amountWidget > 3) {
+        throw new Error('1 to 3 widgets are expected in the header.');
+      }
+    }
 
-    // Example rule 3: If there is a footer, it should contain at least one widget or not more than 3 widget
+    // Example rule 3: If there is a footer, it should contain at least one widget and not more than 3 widget
+    if (pageStructure.page.footer) {
+      const amountWidget = pageStructure.page.footer.widgets.length;
+      if (amountWidget < 1 || amountWidget > 3) {
+        throw new Error('1 to 3 widgets are expected in the footer.');
+      }
+    }
 
     // Example rule 4: If there is a body, it should contain at least one widget
+    if (pageStructure.page.body) {
+      if (pageStructure.page.body.widgets.length == 0) {
+        throw new Error('At least 1 widget expected in body.');
+      }
+    }
     return true;
   }
 }
