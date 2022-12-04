2956b2a98245e2b6196ed95c1de5b897e117645b
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2022-12-04 15:25:08 +0100

    delete widgets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,6 @@ import { HeaderComponent } from './cpe/header/header.component';
 import { BodyComponent } from './cpe/body/body.component';
 import { FooterComponent } from './cpe/footer/footer.component';
 import {GridsterModule} from "angular-gridster2";
-import { Widget1Component } from './widgets/widget1/widget1.component';
-import { Widget2Component } from './widgets/widget2/widget2.component';
-import { Widget3Component } from './widgets/widget3/widget3.component';
 import { TestWidgetComponent } from './cpe/widgets/test-widget/test-widget.component';
 import { ExcecutorComponent } from './cpe/blue-boxes/excecutor/excecutor.component';
 import { AnotherTestWidgetComponent } from './cpe/widgets/another-test-widget/another-test-widget.component';
@@ -22,9 +19,6 @@ import { AnotherTestWidgetComponent } from './cpe/widgets/another-test-widget/an
     HeaderComponent,
     BodyComponent,
     FooterComponent,
-    Widget1Component,
-    Widget2Component,
-    Widget3Component,
     TestWidgetComponent,
     ExcecutorComponent,
     AnotherTestWidgetComponent
