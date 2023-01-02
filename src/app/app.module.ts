4290ff816cef35dffc2aeb6466a48a8140211d38
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2023-01-02 18:24:19 +0100

    fix gridster height

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CpeComponent } from './cpe/cpe.component';
-import {GridsterModule} from 'angular-gridster2';
+import { GridsterModule } from 'angular-gridster2';
 import { TestWidgetComponent } from './cpe/widgets/test-widget/test-widget.component';
 import { ExcecutorComponent } from './cpe/blue-boxes/excecutor/excecutor.component';
 import { AnotherTestWidgetComponent } from './cpe/widgets/another-test-widget/another-test-widget.component';
+import { GuiElementsModule } from "./gui-elements-module/gui-elements.module";
+import { PlaygroundModule } from "./playground/playground.module";
 
 @NgModule({
   declarations: [
