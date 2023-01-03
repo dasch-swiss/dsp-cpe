a53e107493b60899c75e1ef706b3720f27d3e1eb
domsteinbach <dominique.steinbach@unibas.ch>
2023-01-03 15:04:43 +0100

    feat (routing): Change routing via parameter

--- src/app/routing-module/app-routing.module.ts
+++ src/app/routing-module/app-routing.module.ts
@@ -1,20 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CpeComponent } from '../cpe/cpe.component';
+import {ProjectComponent} from "../cpe/project/project.component";
+import {CpeComponent} from "../cpe/cpe.component";
+import {ProjectsComponent} from "../cpe/projects/projects.component";
 
-const routes: Routes = [
+const ROUTES: Routes = [
   {
-    path: '',
+    path: "home",
     component: CpeComponent
   },
   {
-    path: ':edit',
-    component: CpeComponent
+    path: "projects",
+    component: ProjectsComponent,
+  },
+  {
+    path: "project/:id",
+    component: ProjectComponent,
+  },
+  {
+    path: "project/:id/:pageId",
+    component: ProjectComponent,
+  },
+  {
+    path: "**",
+    pathMatch: "full",
+    redirectTo: "/home"
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
+  imports: [RouterModule.forRoot(ROUTES, { onSameUrlNavigation: 'reload' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
