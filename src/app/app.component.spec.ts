2d8ae6a1ece66d6f8dc272fd0753755afa1848c0
mdelez <60604010+mdelez@users.noreply.github.com>
2023-01-09 12:07:14 +0100

    test(app): remove outdated test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,9 +30,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dsp-cpe'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('dsp-cpe');
-  });
 });
