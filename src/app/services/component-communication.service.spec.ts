c6895459683cf0b6b5c1d222a449889580518162
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2023-01-27 10:57:38 +0100

    test(unittests): adapt and add test

--- src/app/services/component-communication.service.spec.ts
+++ src/app/services/component-communication.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ComponentCommunicationService, CpeEvent, Events } from './component-communication.service';
+import { ComponentCommunicationService, CpeEvent, Events, Status } from './component-communication.service';
 
 describe('ComponentCommunicationService', () => {
     let service: ComponentCommunicationService;
@@ -14,16 +14,31 @@ describe('ComponentCommunicationService', () => {
         expect(service).toBeTruthy();
     });
 
-    it('should listen for events and execute code in the callback when the event is emitted', () => {
+    it('should listen for events and execute code in the callback when the event is emitted and status is finished', () => {
         let searchTerm = '';
 
         // listen for searchExecuted event
         service.on(Events.searchExecuted, (e: any) => searchTerm = e);
 
-        // emit searchExecuted event
-        service.emit(new CpeEvent(Events.searchExecuted, 'test'));
+        // emit searchExecuted event with status finished
+        service.emit(new CpeEvent(Events.searchExecuted, Status.finished, 'test'));
 
         expect(searchTerm).toEqual('test');
+
+    });
+
+    it('should listen for events and execute code in the callback when the event is emitted and status is starting', () => {
+        let loading = false;
+
+        // listen for searchExecuted event
+        service.on(Events.searchExecuted, (e: any) => {}, () => {
+            loading = true;
+        });
+
+        // emit searchExecuted event with status starting
+        service.emit(new CpeEvent(Events.searchExecuted, Status.starting));
+
+        expect(loading).toEqual(true);
 
     });
 });
