7610a75442b8e0047eaead0cb2325cee81ad53a4
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2023-02-14 16:37:55 +0100

    refactor: fix structure

--- src/app/cpe/model/page-data-structure.ts
+++ src/app/cpe/model/page-data-structure.ts
@@ -1,60 +1,53 @@
 export class Project {
-    private _pages: Page[] = []
-
-    set pages(pages: Page[]) {
-        this._pages = pages;
-    }
-
-    get pages(): Page[] {
-        return this._pages;
-    }
-
-    hasPage(pageID: string): boolean {
-        return this._pages.some((page:Page) => page.hasPageID(pageID));
-    }
-
-    constructor(public readonly id: string,
-                public label: string,
-                public description: string,
-                public mainPageID: string
-    ) {
-    }
-}
-
-export class Page {
     private _header: Widget[] = [];
-    private _body: Widget[] = [];
+    private _body: Page[] = [];
     private _footer: Widget[] = [];
 
     get header(): Widget[] {
         return this._header;
     }
-
     set header(header: Widget[]) {
         this._header = header;
     }
-
-    get body(): Widget[] {
+    get body(): Page[] {
         return this._body;
     }
-
-    set body(body: Widget[]) {
+    set body(body: Page[]) {
         this._body = body;
     }
-
     get footer(): Widget[] {
         return this._footer;
     }
-
     set footer(footer: Widget[]) {
         this._footer = footer;
     }
+    hasPage(pageID: string): boolean {
+        return this._body.some((page:Page) => page.hasPageID(pageID));
+    }
 
+    constructor(public readonly id: string,
+                public label: string,
+                public description: string,
+                public mainPageID: string,
+                public gridDimensions: Dimension
+    ) {
+    }
+}
+
+export class Page {
+    private _widgets: Widget[] = [];
+
+    get widgets(): Widget[] {
+        return this._widgets;
+    }
+    set widgets(widgets: Widget[]) {
+        this._widgets = widgets;
+    }
     hasPageID(pageID: string): boolean {
         return this.id === pageID;
     }
 
-    constructor(public readonly id: string, public gridDimensions: Dimension) {
+    constructor(public readonly id: string) {
     }
 }
 
@@ -65,7 +58,6 @@ export class Widget {
     get dimension() {
         return this._dimension;
     }
-
     get coordinates() {
         return this._coordinates;
     }
--- src/app/cpe/validator/page-structure-validator.service.ts
+++ src/app/cpe/validator/page-structure-validator.service.ts
@@ -10,47 +10,33 @@ export class PageStructureValidatorService {
 
     validate(projectStructure: Project) {
         // Example rule 1: It should have at least a page
-        if (projectStructure.pages.length === 0) {
-            throw Error("It should have at least a page in the project");
+        if (projectStructure.body.length === 0) {
+            throw Error("It should have at least a page in the body");
         }
 
         // Example rule 2: All pages should ...
-        // ... contain at least a header or a body or a footer
-        for (let page of projectStructure.pages) {
-            if (page.header.length === 0 && page.body.length === 0 || page.footer.length === 0) {
-                throw Error("It should have a header, body or a footer");
-            }
-
-            // Example rule 2a: If there is a header, it should ...
-            // ... contain not more than 3 widgets
-            if (page.header.length > 3) {
-                throw Error("Not more than 3 widgets in the header");
-            }
-
-            // Example rule 2b: If there is a footer, it should ...
-            // ... contain not more than 3 widgets
-            if (page.footer.length > 3) {
-                throw Error("Not more than 3 widgets in the footer");
-            }
+        // ... contain at least a widget in the header and in the footer
+        if (projectStructure.header.length == 0 || projectStructure.footer.length == 0) {
+            throw Error("It should have a header and a footer");
         }
 
-        // Example rule 3: Header widgets should be placed above footer widgets
-        for (let page of projectStructure.pages) {
-            if (page.header.length > 0 && page.footer.length > 0) {
-
-                let found = false;
-                for (let w_h of page.header) {
-                    for (let w_f of page.footer) {
-                        if (w_h.coordinates.y >= w_f.coordinates.y) {
-                            found = true
-                        }
-                    }
-                }
-
-                if (found) {
-                    throw Error("Header widgets not placed above footer widgets");
-                }
-            }
-        }
+        // // Example rule 3: Header widgets should be placed above footer widgets
+        // for (let page of projectStructure.pages) {
+        //     if (page.header.length > 0 && page.footer.length > 0) {
+        //
+        //         let found = false;
+        //         for (let w_h of page.header) {
+        //             for (let w_f of page.footer) {
+        //                 if (w_h.coordinates.y >= w_f.coordinates.y) {
+        //                     found = true
+        //                 }
+        //             }
+        //         }
+        //
+        //         if (found) {
+        //             throw Error("Header widgets not placed above footer widgets");
+        //         }
+        //     }
+        // }
     }
 }
