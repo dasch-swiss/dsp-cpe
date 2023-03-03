3c00760e463196d1138984089c5c0b0a896e9c84
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2023-03-03 11:26:02 +0100

    style: search results

--- src/app/cpe/widgets/search-widget/search-widget.component.ts
+++ src/app/cpe/widgets/search-widget/search-widget.component.ts
@@ -19,9 +19,9 @@ export class SearchWidgetComponent implements OnInit {
     getMockData(): Observable<{ id: number, title: string }[]> {
         return of([
             {id: 1, title: "Richard III", published: "1593", author: "William Shakespeare", passage: "[Richard:] A horse, a horse, my kingdom for a horse!"},
-            {id: 2, title: "What You Will", published: "1601", author: "John Marston", passage: "[Lampatho Doria:] Nay, hear it, and relish it judiciously.<br>Quadratus: I do relish it most judiciously.<br>[Lampatho Doria:] Adored excellence, delicious sweet <br>[Quadratus:] \"Delicious sweet\"! good, very good."},
-            {id: 3, title: "Hamlet", published: "1600", author: "William Shakespeare", passage: "[Hamlet:] [...] my two schoolfellows,<br> Whom I will trust as I will adders fanged,<br>They bear the mandate; they must sweep my way<br>And marshal me to knavery."},
-            {id: 4, title: "The Tragedy of Charles, Duke of Byron", published: "1608", author: "George Chapmann", passage: "[Captain:] And to make this no less than an Ostent;<br>Another that hath fortuned since, confirms it:<br>Your goodly horse Pastrana, which the Archduke,<br>Gave you at Brussels; in the very hour,<br>You left your strength, fell mad, and killed himself;<br>The like chanced to the horse the great duke sent you:<br>And, with both these, the horse the duke of Lorraine,<br>Sent you at, Vimie made a third presage, [...]<br>Who like the other pined away and died. "}
+            {id: 2, title: "The Tragedy of Charles, Duke of Byron", published: "1608", author: "George Chapmann", passage: "[Captain:] And to make this no less than an Ostent;<br>Another that hath fortuned since, confirms it:<br>Your goodly horse Pastrana, which the Archduke [...]"},
+            {id: 3, title: "What You Will", published: "1601", author: "John Marston", passage: "[Lampatho Doria:] Nay, hear it, and relish it judiciously.<br>Quadratus: I do relish it most judiciously.<br>[Lampatho Doria:] Adored excellence, delicious sweet <br>[Quadratus:] \"Delicious sweet\"! good, very good."},
+            {id: 4, title: "Hamlet", published: "1600", author: "William Shakespeare", passage: "[Hamlet:] [...] my two schoolfellows,<br> Whom I will trust as I will adders fanged,<br>They bear the mandate; they must sweep my way"}
         ]).pipe(delay(1000));
     }
 
